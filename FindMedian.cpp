ba705c6c9cb6469fac966522f36d45f0478e4bd7
janahindiyeh <janahindiyeh@umail.ucsb.edu>
2021-02-19 00:45:13 +0200

    Add files via upload

--- FindMedian.cpp
+++ FindMedian.cpp
@@ -8,12 +8,6 @@ using namespace std;
 
 void FindMedian(string filename, ifstream& ifs);
 
-int main() { 
-ifstream ifs; 
-string fname = "Num.txt"; 
-FindMedian(fname, ifs);
-return 0;
-}
 
 void FindMedian(string filename, ifstream& ifs){
 
@@ -22,20 +16,25 @@ void FindMedian(string filename, ifstream& ifs){
         cout << "Input file opening failed.\n";
         exit(1);
     }
-    int num1(0);
-    ifs >> num1;
 
     ofstream ofs;
     ofs.open("median_output.dat");
     if (ofs.fail( )){
         cout << "Output file opening failed.\n";
         exit(1);
- }
+    }
+
+    int num;
+    int size(0), i(0);
+   while(ifs >> num){
+       size++;
+   }
+
+   ifs.close();
+
+   ifs.open(filename.c_str());
 
    int temp = 0;
-   int size = 0;
-   int num;
-   int i = 0;
 
    int arr[size];
 
@@ -54,16 +53,15 @@ void FindMedian(string filename, ifstream& ifs){
            }
        }      
    
-   int m = size/2;      
+   int m = size/2;    
    
    if(size % 2 == 0){    
        ofs << (arr[m-1] + arr[m])/2 << endl;
-       cout << (arr[m-1] + arr[m])/2;
    }
 
    else{     
        ofs << arr[m] << endl; 
-       cout << arr[m];
    }   
     
 }
+
